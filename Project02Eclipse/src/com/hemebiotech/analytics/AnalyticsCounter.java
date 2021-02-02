e963d626e03e07b295dc278e047eda294608e6c2
SamVerdiere <Samverdiere@gmail.com>
2021-02-02 22:33:15 +0100

    début fix

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -11,7 +11,7 @@ public class AnalyticsCounter {
 	
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		BufferedReader reader = new BufferedReader (new FileReader("C:\\Users\\ADMIN\\IdeaProjects\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\src\\resources\\symptoms.txt"));
 		String line = reader.readLine();
 
 		int i = 0;	// set i to 0
